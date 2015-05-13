f07bf13703f2bc41ffd61a11ccedfac18936fbbd
Rob Matwiejczyk <rob@parall.ax>
2015-05-13 22:41:32 +0100

    typography stuffs

--- app/wp-content/themes/labster/front-page.php
+++ app/wp-content/themes/labster/front-page.php
@@ -4,15 +4,19 @@
 	<div class="area__inner">
 
 <?php 
-	$music = new WP_Query("posts_per_page=-1&orderby=post_date&order=DESC&post_status=publish");
+	$music = new WP_Query("posts_per_page=5&orderby=post_date&order=DESC&post_status=publish");
 
 	if($music->have_posts()) :
 	  while($music->have_posts()) :
 	    $music->the_post();
 	?>
 
-	        <section class="post" data-slug="<?php echo $post->post_name; ?>">
-	        	<h1><?php the_title(); ?></h1>
+	        <section class="post">
+	        	<h1>
+	        		<a href="<?php echo $post->post_name; ?>">
+	        			<?php the_title(); ?>
+	        		</a>
+	        	</h1>
 	        </section>
 
 	<?php 
